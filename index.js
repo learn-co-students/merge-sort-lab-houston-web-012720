a7b90ce230d6b6368369524741a54148b7aa3064
SteRobWms <stephen.r.williams.87@gmail.com>
2020-08-02 21:23:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(firstArray, secondArray) {
+    let newArr = []
+    while (firstArray.length !== 0 && secondArray.length !== 0) {
+        if (firstArray[0] <= secondArray[0]) {
+            newArr.push(findMinAndRemoveSorted(firstArray))
+        }
+        else {
+            newArr.push(findMinAndRemoveSorted(secondArray))
+        }
+    }
+    if (firstArray.length === 0) {
+        newArr = newArr.concat(secondArray)
+    }
+    else {
+        newArr = newArr.concat(firstArray)
+    }
+    return newArr
+}
+
+function mergeSort(array) {
+    let midpoint = array.length / 2
+    let firstArray = array.slice(0, midpoint)
+    let secondArray = array.slice(midpoint, array.length)
+
+    if (array.length < 2) {
+        return array
+    }
+    else {
+        return merge(mergeSort(firstArray), mergeSort(secondArray))
+    }
+}
